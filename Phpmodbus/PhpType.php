b64f90143f85825d0dd4e50d22a6c193de923b65
Pavel <omikronixz@lover.ee>
2015-10-16 15:55:06 +0300

    Update PhpType.php
    combineBytes fix

--- Phpmodbus/PhpType.php
+++ Phpmodbus/PhpType.php
@@ -229,19 +229,23 @@ class PhpType {
      * @param bool $endianness
      * @return int
      */
-    private static function combineBytes($data, $endianness) {
+    private static function combineBytes($data, $endianness = 0) {
         $value = 0;
         // Combine bytes
         if ($endianness == 0)
-            $value = (($data[3] & 0xFF)<<16) |
-                    (($data[2] & 0xFF)<<24) |
+        {
+            // big endian
+            $value = (($data[3] & 0xFF) << 24) |
+                    (($data[2] & 0xFF) << 16) |
                     (($data[1] & 0xFF)) |
-                    (($data[0] & 0xFF)<<8);
-        else
-            $value = (($data[3] & 0xFF)<<24) |
-                    (($data[2] & 0xFF)<<16) |
-                    (($data[1] & 0xFF)<<8) |
-                    (($data[0] & 0xFF));
+                    (($data[0] & 0xFF) << 8);
+        } else {
+            // little endian
+            $value = (($data[0] & 0xFF) << 24) |
+                    (($data[1] & 0xFF) << 16) |
+                    (($data[2] & 0xFF) << 8) |
+                    (($data[3] & 0xFF));
+        }
 
         return $value;
     }
